5eccfaec8404a4d7310ad64e9c43da54011bcc27
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-08 00:10:22 +0530

    Adding factorial code using long datatyes checking overflow

---
+++ src/revesion/ForFactorial.java
@@ -0,0 +1,14 @@
+package revesion;
+import java.util.Scanner;
+public class ForFactorial {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner input = new Scanner(System.in);
+         long num  = input.nextInt();
+         long  fact = 1;
+        for(int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Fact = " + fact );
+    }
+}
