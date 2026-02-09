725ca8bf847bca2401769fa36dad39b9afe66b0b
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-09 22:29:15 +0530

    add square and cube power code

---
+++ src/revesion/Square.java
@@ -0,0 +1,15 @@
+package revesion;
+import java.util.Scanner;
+public class Square {
+    public static void main(String[] args) {
+        System.out.println("Enter a integer: ");
+        Scanner d = new Scanner(System.in);
+        int num = d.nextInt();
+        int square = num * num;
+        System.out.println("Square = " + square);
+        Scanner input = new Scanner(System.in);
+        int cube = input.nextInt();
+        int cube3 = cube * cube * cube;
+        System.out.println("Cube = " + cube3);
+    }
+}
