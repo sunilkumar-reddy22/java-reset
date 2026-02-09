340a32a75560124e8a7f5262c349ac3d2f09932f
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-09 22:06:51 +0530

    Add character program

---
+++ src/revesion/TypecastingRevise.java
@@ -0,0 +1,17 @@
+package revesion;
+import java.util.Scanner;
+public class TypecastingRevise{
+    public static void main(String[] args) {
+        System.out.println("Enter a character: ");
+        Scanner letter  = new Scanner(System.in);
+        char ch = letter.next().charAt(2);
+        if(ch >= 'a'  && ch <= 'z'){
+            System.out.println("lowercase");
+        }
+        else{
+            System.out.println("uppercase");
+
+        }
+    }
+
+}
