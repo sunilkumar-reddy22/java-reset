1bc11dc5a0716800413e53feac60fe2fd93fb621
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-11 00:13:38 +0530

    math -table of 5 to 15

---
+++ src/revesion/mathtables5to15.java
@@ -0,0 +1,14 @@
+package revesion;
+
+public class mathtables5to15 {
+    public static void main(String[] args) {
+        for (int num = 5; num <= 15; num++) {
+            System.out.println("table of = " + num);
+
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(num + " x " + j + " = " + (num * j));
+            }
+            System.out.println();
+        }
+    }
+}
