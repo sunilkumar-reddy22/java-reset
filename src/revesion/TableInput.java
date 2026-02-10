ad6fe23012f157714449f9bc2673527587054ae3
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-10 23:21:17 +0530

    Infinite math tables using while loop

---
+++ src/revesion/TableInput.java
@@ -0,0 +1,14 @@
+package revesion;
+import java.util.Scanner;
+public class TableInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n Enter a number: ");
+            int table = input.nextInt();
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(table + " x  " + i + " = " + (table * i));
+            }
+        }
+    }
+}
