54392ae4e81d0c80a250ae32bb4c425d09244194
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-10 23:31:33 +0530

    revesion of math table

---
+++ src/revesion/Usertable.java
@@ -0,0 +1,12 @@
+package revesion;
+import java.util.Scanner;
+public class Usertable {
+    public static void main(String[] args) {
+        System.out.println("enter an integer: ");
+        Scanner input = new Scanner (System.in);
+        int k = input.nextInt();
+        for(int j = 1; j <= 10;  j++) {
+            System.out.println(k + " x " + j + " = " + (k * j));
+        }
+    }
+}
