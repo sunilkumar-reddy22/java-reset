69af7643bf27560620080fe4b458705c189fe98a
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-10 23:52:44 +0530

    Math tables from 1 to 20

---
+++ src/revesion/Mathtables1to20.java
@@ -0,0 +1,15 @@
+package revesion;
+
+public class Mathtables1to20 {
+    public static void main(String[] args) {
+        for(int num = 1; num <= 20; num++) {
+            System.out.println("Table of = " + num);
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+            }
+            System.out.println();
+        }
+
+    }
+}
