b2c2027ddcd79285c51a83147445eaf66574df9a
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-02-12 23:21:40 +0530

    iwant to write it

---
+++ src/revesion/Comeback.java
@@ -0,0 +1,9 @@
+package revesion;
+
+public class Comeback {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 3; i++) {
+            System.out.println("I'm back,I am gonna achive my goals");
+        }
+    }
+}
