fdea0fe7698bc0b9a5e8d5a2db2fe3cefb876de4
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-04 15:07:54 +0530

    git commit -m "Revision,couunt digits patteren"

--- src/ProjectMini.java
+++ src/ProjectMini.java
@@ -1,7 +1,17 @@
-public class ProjectMini {
 import java.util.Scanner;
+public class ProjectMini {
 
     public static void main(String[] args) {
+        System.out.println("Enter Number: ");
+        Scanner vision = new Scanner(System.in);
+        int x = vision.nextInt();
+        int count = 0;
+        while( x > 0) {
+            int rem = x % 10;
+            count++;
+            x = x / 10;
+        }
+        System.out.println(count);
 
     }
 }
