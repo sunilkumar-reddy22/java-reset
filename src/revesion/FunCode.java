2ad5a2a54ae249fa299923bdbd9d45216bc9e804
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-07 22:37:03 +0530

    changed print method what to execute first

--- src/revesion/FunCode.java
+++ src/revesion/FunCode.java
@@ -2,18 +2,21 @@ package revesion;
 import java.util.Scanner;
 public class FunCode {
     public static void main(String[] args) {
-        sum();
-        sum();
+    int result0 = sum();
+        System.out.println("Result = " +  result0);
+    int result1 = sum();
+        System.out.println(result1);
         System.out.println("get back to main");
     }
-    static void sum() {
+    static int sum() {
     Scanner input = new Scanner(System.in);
         System.out.println("Enter first number:");
         int a = input.nextInt();
         System.out.println("Enter second number");
         int b = input.nextInt();
-        int sum = a + b;
-        System.out.println("sum =  " + sum );
+        int result = a + b;
+     //   System.out.println("sum =  " + sum );
+     return result;
 
     }
 }
