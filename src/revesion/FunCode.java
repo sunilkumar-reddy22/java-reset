a640578eafcccf18f820e5741c785b49f4479d60
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-08 08:40:59 +0530

    added Infinite loop,and condition valadion and to break the block with 0 vakue

--- src/revesion/FunCode.java
+++ src/revesion/FunCode.java
@@ -2,21 +2,33 @@ package revesion;
 import java.util.Scanner;
 public class FunCode {
     public static void main(String[] args) {
-    int result0 = sum();
-        System.out.println("Result = " +  result0);
-    int result1 = sum();
+
+    while (true) {
+        int result0 = sum();
+        if(result0 == 0) {
+            System.out.println("stop");
+            break;
+        }
+        System.out.println("Result = " + result0);
+        int result1 = sum();
         System.out.println(result1);
         System.out.println("get back to main");
     }
+    }
     static int sum() {
-    Scanner input = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int a = input.nextInt();
-        System.out.println("Enter second number");
-        int b = input.nextInt();
-        int result = a + b;
-     //   System.out.println("sum =  " + sum );
-     return result;
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter first number:");
+            int a = input.nextInt();
+            if (a == 0) {
+                System.out.println("Invalid input");
+                return 0;
+            }
+            System.out.println("Enter second number");
+            int b = input.nextInt();
+            int result = a + b;
+            //   System.out.println("sum =  " + sum );
+            return result;
+        }
 
     }
-}
+
