e859fb42001669615f1ab0fe9b9c481d1687b252
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-19 23:14:55 +0530

    sample prime printing prime numbers accordingly

---
+++ src/revesion/PrimeCheckVtwo.java
@@ -0,0 +1,27 @@
+package revesion;
+import java.util.Scanner;
+public class PrimeCheckVtwo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int n = in.nextInt();
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+
+    }
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num %  2 == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
---
+++ src/revesion/PrimeNum1.java
@@ -0,0 +1,26 @@
+package revesion;
+import java.util.Scanner;
+public class PrimeNum1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int n = in.nextInt();
+        boolean  result =  isPrime(n);
+        System.out.println("Result is " + result);
+
+     //   System.out.println("Result = " + isPrime);
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+           // return true;
+        }
+        return true;
+    }
+}
