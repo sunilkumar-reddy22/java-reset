6cfb0c86e29e61b66a6755048ea03ac22cfa181a
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-21 12:22:33 +0530

    Prcticing this method to recall the pattern like a exercgit

---
+++ src/revesion/PythageroanTriples.java
@@ -0,0 +1,31 @@
+package revesion;
+import java.util.Scanner;
+public class PythageroanTriples {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+        System.out.println("First Number: ");
+        int a = in.nextInt();
+        System.out.println("Second Number: ");
+        int b = in.nextInt();
+        System.out.println("Third Number: ");
+        int c =  in.nextInt();
+       if(isprime(a,b,c)) {
+           System.out.println("Is Prime");
+       }
+       else {
+           System.out.println("Is not prime");
+       }
+    }
+    static boolean isprime(int a,int b,int c) {
+        int max = Math.max(a, Math.max(b,c));
+        if(max == a) {
+           return a * a == b * b + c * c;
+        }
+        else if(max == b) {
+            return b * b == c * c + a * a;
+        }
+        else {
+            return c * c == a * a + b * b;
+        }
+    }
+}
