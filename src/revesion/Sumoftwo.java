d331adf53cd23d297942c386c26fcef15ac6a010
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-08 22:44:17 +0530

    sum of nums usinng nums revison

---
+++ src/revesion/Sumoftwo.java
@@ -0,0 +1,11 @@
+package revesion;
+
+public class Sumoftwo {
+    public static void main(String[] args) {
+     int ans =  nums(20);
+        System.out.println(ans);
+    }
+    static void nums(int a) {
+        a = 15;
+    }
+}
